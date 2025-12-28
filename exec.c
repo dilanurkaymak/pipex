99269c1e354a860ceba56087d891416496349bd1
Dilanur Nigar Kaymak <dkaymak@k1m04s02.42istanbul.com.tr>
2025-12-28 18:04:46 +0300

    güncelleme2

--- exec.c
+++ exec.c
@@ -6,11 +6,17 @@
 /*   By: dkaymak <dkaymak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/28 17:36:30 by dkaymak           #+#    #+#             */
-/*   Updated: 2025/12/28 17:36:31 by dkaymak          ###   ########.fr       */
+/*   Updated: 2025/12/28 18:04:14 by dkaymak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+static void	cmd_not_found(void)
+{
+	write(2, "pipex: command not found\n", 27);
+	exit(127);
+}
 
 void	exec_cmd(char *cmd, char **envp)
 {
@@ -18,16 +24,22 @@ void	exec_cmd(char *cmd, char **envp)
 	char	*path;
 
 	if (!cmd || !*cmd)
-		error_exit("command not found", 127);
+		cmd_not_found();
 	args = ft_split(cmd, ' ');
+	if (!args || !args[0])
+	{
+		ft_free_split(args);
+		cmd_not_found();
+	}
 	path = find_path(args[0], envp);
 	if (!path)
 	{
 		ft_free_split(args);
-		error_exit("command not found", 127);
+		cmd_not_found();
 	}
 	execve(path, args, envp);
+	perror("pipex");
 	ft_free_split(args);
 	free(path);
-	error_exit("execve", 126);
+	exit(126);
 }
